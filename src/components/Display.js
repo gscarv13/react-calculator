8b7b8e8d6f4fc9e12f767bc3c149986d8830e2b4
Gus <gsc13@tuta.io>
2021-07-01 16:40:35 -0300

    Add prop type and default prop value to Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Display() {
+function Display(props) {
+  const { result } = props;
   return (
     <div>
-      Display placeholder
+      Result is
+      { ` ${result}` }
     </div>
   );
 }
+
+Display.defaultProps = {
+  result: '0',
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
 
 export default Display;
