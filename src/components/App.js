749fed9b3447e87a347d0c19c692ff47c0347ca6
Gustavo Carvalho <gsc13@tuta.io>
2021-07-06 11:38:00 -0300

    Update App functional component into class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
 import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div>
-      <p>Calculator</p>
-      <>
-        <Display />
-        <ButtonPanel />
-      </>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(btnName) {
+    const obj = { ...this.state };
+    this.setState({ total: Calculate(obj, btnName) });
+  }
+
+  render() {
+    const { total, next } = this.state;
+    const current = next || total;
+    return (
+      <div>
+        <>
+          <Display result={current} />
+          <ButtonPanel onClick={this.handleClick} />
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
