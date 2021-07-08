2c9183ac56a442e0e5dd11a50ca734e69a5a3922
Gus <gsc13@tuta.io>
2021-07-08 19:10:46 -0300

    Refactor App class component into functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      // eslint-disable-next-line react/no-unused-state
-      operation: null,
-    };
-  }
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-  handleClick(btnName) {
-    const obj = this.state;
+  const handleClick = (btnName) => {
+    const obj = { total, next, operation };
     const result = Calculate(obj, btnName);
-    this.setState(result);
-  }
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+  };
 
-  render() {
-    const { total, next } = this.state;
-    const current = next || total || '0';
-    return (
-      <div>
-        <>
-          <Display result={current} />
-          <ButtonPanel clickHandler={this.handleClick} />
-        </>
-      </div>
-    );
-  }
-}
+  const current = next || total || '0';
+  return (
+    <div>
+      <>
+        <Display result={current} />
+        <ButtonPanel clickHandler={handleClick} />
+      </>
+    </div>
+  );
+};
 
 export default App;
