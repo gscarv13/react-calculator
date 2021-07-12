03a8202496659a4f3bfb7d8a6919b3cd10498c46
Gus <gsc13@tuta.io>
2021-07-12 17:33:35 -0300

    Remove Home.css, Add new stylings

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from './Header';
-import '../styles/Home.css';
 
 const Home = () => (
   <>
     <Header />
     <div className="right">
-      <div className="home">
+      <div className="box">
         <h2>About us</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita libero
+          necessitatibus neque obcaecati.Ad, assumenda autem, corporis eligendi ex
+          explicabo maiores minima obcaecati possimus, quisquam sequi ut voluptatem voluptatum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita libero
+          necessitatibus neque obcaecati.Ad, assumenda autem, corporis eligendi ex
+          explicabo maiores minima obcaecati possimus, quisquam sequi ut voluptatem voluptatum.
+        </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita libero
           necessitatibus neque obcaecati.Ad, assumenda autem, corporis eligendi ex
