fb79ebcc0f7a802e9f0eb24a8b3cd3c55319e2bb
Gus <gsc13@tuta.io>
2021-07-12 16:50:38 -0300

    Finish Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Header from './Header';
+import '../styles/Home.css';
 
 const Home = () => (
-  <div>
-    <h1>Math Magician</h1>
-    <p>Welcome to our page!</p>
-  </div>
+  <>
+    <Header />
+    <div className="right">
+      <div className="home">
+        <h2>About us</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita libero
+          necessitatibus neque obcaecati.Ad, assumenda autem, corporis eligendi ex
+          explicabo maiores minima obcaecati possimus, quisquam sequi ut voluptatem voluptatum.
+        </p>
+      </div>
+    </div>
+  </>
 );
 
 export default Home;
