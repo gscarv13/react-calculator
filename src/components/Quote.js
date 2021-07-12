9808ba03f178443d7d8f76d49993fbbd8a10a24e
Gus <gsc13@tuta.io>
2021-07-12 17:33:01 -0300

    Add Quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,18 @@
+import Header from './Header';
+
+const Quote = () => (
+  <>
+    <Header />
+    <div className="right">
+      <div className="box">
+        <p className="quote">
+          ❝Mathematics is not about numbers, equations, computations,
+          or algorithms: it is about understanding.❞
+        </p>
+        <p className="author"> William Paul Thurston </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Quote;
